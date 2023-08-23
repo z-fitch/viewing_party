5f91362ce8665a9d17d64ad26509c2d815564a31
Zanna Fitch <zannafitch2004@gmail.com>
2023-08-23 11:39:50 -0500

    Test: add tests for creating a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
-    it { should validate_uniqueness_of(:email).on(:create) }
+    it { should validate_uniqueness_of(:email) }
   end
 end
