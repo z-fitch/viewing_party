5a26248ceabc912240dc154ab2c07fe70503d95f
Weston Schutt <wtschutt@gmail.com>
2023-08-25 16:40:18 -0600

    Test: Add tests for movie poro instance methods and VCR casset fixtures

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Movie do
   it 'exists' do
-    attributes = {
+    attributes = attributes = {
       id: 1234,
       title: 'Clueless',
       vote_average: 8.7
